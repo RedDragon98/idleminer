e5d945346d02c283589ed855fe16a3471cef4984
RedDragon98 <sritest@live.com>
2021-11-01 15:39:01 -0700

    implemented resource class

--- resources.py
+++ resources.py
@@ -1,18 +1,31 @@
-"""implments resource class"""
+"""implements resource class"""
 
 
-class Resources:  # TODO
+class Resources:
     """manages different resource collections(like inventory)"""
 
     def __init__(self):
-        pass
+        self.resdict = {}
 
     def modify(self, resource: str, amount: int):
         """adds or removes resources"""
+        if resource in self.resdict:
+            self.resdict[resource] += amount
+        else:
+            self.resdict[resource] = amount
+
+    def get(self, resource):
+        """gets amount of resources"""
+        return self.resdict[resource]
 
     def save(self) -> dict:
         """returns dict representation"""
+        return self.resdict
 
 
 def load(resdict: dict) -> Resources:
     """load from dict representation"""
+    res = Resources()
+    res.resdict = resdict
+
+    return res
